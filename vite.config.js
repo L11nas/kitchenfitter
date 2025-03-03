ea9efcf2f31f35a21a5b8f40a8a9f462b921a80d
L11nas <ulevicius.linas@gmail.com>
2025-03-03 19:47:59 +0200

    change vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // 🔥 Užtikrina, kad maršrutai veiktų tinkamai
 });
