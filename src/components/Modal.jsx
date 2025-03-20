fa396774f72e2e29de108dc5c0208d0f7cc591f2
L11nas <ulevicius.linas@gmail.com>
2025-03-20 20:34:32 +0200

    modal.css

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './styles/Modal.css'; // Patikrink, ar šiame faile nėra `target="_blank"`
+import './styles/modal.css'; // Patikrink, ar šiame faile nėra `target="_blank"`
 
 export default function Modal({
   isOpen,
